7a77e561f89acd14d93a7f46a0e11000dbf065ae
t0nyandre <post@tonyandre.co>
2019-02-04 13:04:26 +0100

    Changed from <button> to <a>

--- src/utils/mails/verifyAccountMail.ts
+++ src/utils/mails/verifyAccountMail.ts
@@ -1,12 +1,14 @@
 import { User } from "../../models/User";
 import { serverURL } from "../../../config/default";
+// tslint:disable-next-line
+require("dotenv").config();
 
 export const verifyAccountMail = (user: User, token: string) => {
   const mail = {
-    from: "Tony André Haugen <no-reply@tonyandre.co>",
+    from: `${process.env.FROM_NAME} <${process.env.FROM_MAIL}>`,
     to: user.email,
     subject: `Thank you, ${user.username} for signing up to my website!`,
-    html: `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">\n<html lang="en">\n<head>\n<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">\n<meta name="viewport" content="width=device-width, initial-scale=1">\n<meta http-equiv="X-UA-Compatible" content="IE=edge">\n<title></title>\n<style type="text/css"></style>\n</head>\n<body style="margin:0;padding:0;background-color:#f2f2f2">\n<center>\n<table width="640" cellspacing="0" cellpadding="0" border="0" align="center" style="max-width:640px;width:100%" bgcolor="#FFFFFF">\n<tr>\n<td align="center" valign="top" style="padding:10px">\n<table width="600" cellspacing="0" cellpadding="0" border="0" align="center" style="max-width:600px;width:100%">\n<tr>\n<td align="center" valign="top" style="padding:10px">\n<p>\nThank you for signing up to my website.\n</p>\n<p>\nThere is only one more step required to login;\n</p>\n<form action="${serverURL}/user/verify/${token}" target="_BLANK">\n<button style="padding:10px;cursor:pointer;background-color:cyan;font-weight:bold;border:1px solid #222">\nVERIFY ACCOUNT\n</button>\n</form>\n<p style="font-size:9px;margin-top:20px">\nIf the button doesn\'t work for you click <a href="${serverURL}/user/verify/${token}" target="_BLANK">here</a>.\n</p>\n</td>\n</tr>\n</table>\n</td>\n</tr>\n</table>\n</center>\n</body>\n</html>`,
+    html: `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"><html lang="en"><head> <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <title>Forgot password? No worries!</title> <style type="text/css">a:hover{color:red}</style> </head><body style="margin:0;padding:0;background-color:#f2f2f2;margin-top:35px"> <center> <table width="640" cellspacing="0" cellpadding="0" border="0" align="center" style="max-width:640px;width:100%" bgcolor="#FFFFFF"> <tr> <td align="center" valign="top" style="padding:10px"> <table width="650" cellspacing="0" cellpadding="0" border="0" align="center" style="max-width:650px;width:100%"> <tr> <td align="center" valign="top" style="padding:10px"> <p> Thank you for signing up to my website. </p> <p style="padding-bottom:15px"> There is only one more step required to login: </p> <a href="${serverURL}/user/changepass/${token}" target="_BLANK" style="padding:15px;cursor:pointer;background-color:cyan;font-weight:bold;color:#000;font-size:16px;text-decoration:none;border:1px solid #222"> VERIFY ACCOUNT </a> <p style="font-size:9px;padding-top:20px"> Button doesn't work? Copy & paste this into your browser:<br>${serverURL}/user/changepass/${token} </p> </td> </tr> </table> </td> </tr></table> </center></body></html>`,
   };
   return mail;
 };
